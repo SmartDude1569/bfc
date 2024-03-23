de78fc1dbc38c35d9d950c2f8f358976a1681159
SmartDude <aidanwalker595@gmail.com>
2024-03-23 19:16:20 -0400

    actual interpreter (probably doesnt work)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::process;
+use std::fs;
 
 mod tape;
 use tape::Tape;
@@ -17,6 +18,47 @@ fn main() {
         process::exit(1);
     }
     let src_name = &args[1];
+    let mut src = fs::read_to_string(src_name)
+        .expect("couldn't read the file");
     println!("executing {}", src_name);
-    
+    let mut tape = Tape::<i8>::new(30000);
+    for instruction in unsafe { src.as_mut_vec() } {
+        match instruction {
+            b'>' => {
+                tape.move_right();
+                ()
+            }
+            b'<' => {
+                tape.move_left();
+                ()
+            }
+            b'+' => {
+                tape.increment();
+                ()
+            }
+            b'-' => {
+                tape.decrement();
+                ()
+            }
+            b'.' => {
+                print!("{}", tape.get());
+                ()
+            }
+            b',' => {
+                // anyone who uses input in their program is a pussy
+                ()
+            }
+            b'[' => {
+                // fuck you im not gonna implement this shit
+                ()
+            }
+            b']' => {
+                // ill implement these sanity-fuckers later
+                ()
+            }
+            _ => {
+                ()
+            }
+        }
+    }
 }
--- src/tape.rs
+++ src/tape.rs
@@ -10,38 +10,38 @@ where
     T: Default + Copy + Add<Output = T> + Sub<Output = T>,
 {
     // Create a new Tape with specified memory length
-    fn new(memory_length: usize) -> Self {
+    pub(crate) fn new(memory_length: usize) -> Self {
         Self {
             cells: vec![T::default(); memory_length],
             pointer: 0,
         }
     }
     // Increment the cell at the pointer
-    fn increment(&mut self) {
+    pub(crate) fn increment(&mut self) {
         self.cells[self.pointer] = self.cells[self.pointer] + T::default();
     }
     // Decrement the cell at the pointer
-    fn decrement(&mut self) {
+    pub(crate) fn decrement(&mut self) {
         self.cells[self.pointer] = self.cells[self.pointer] - T::default();
     }
     // Move the pointer to the right
-    fn move_right(&mut self) {
+    pub(crate) fn move_right(&mut self) {
         if self.pointer < self.cells.len() - 1 {
             self.pointer += 1;
         }
     }
     // Move the pointer to the left
-    fn move_left(&mut self) {
+    pub(crate) fn move_left(&mut self) {
         if self.pointer > 0 {
             self.pointer -= 1;
         }
     }
     // Get the value of the cell at the pointer
-    fn get(&self) -> T {
+    pub(crate) fn get(&self) -> T {
         self.cells[self.pointer]
     }
     // Set the value of the cell at the pointer
-    fn set(&mut self, value: T) {
+    pub(crate) fn set(&mut self, value: T) {
         self.cells[self.pointer] = value;
     }
 }
